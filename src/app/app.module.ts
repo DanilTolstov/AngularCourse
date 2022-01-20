8cb36676032f64a0a0a19cf632db4588558b0e04
Danil Tolstov <dfrontman@gmail.com>
2022-01-20 16:23:57 +0300

    Basic angular app, from pluralSight course

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule} from "@angular/forms";
+
+import { AppComponent } from './app.component';
+import { ProductListComponent } from "./products/product-list.component";
+import {ConvertToSpacePipe} from "./shared/convert-to-space.pipe";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductListComponent,
+    ConvertToSpacePipe,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
