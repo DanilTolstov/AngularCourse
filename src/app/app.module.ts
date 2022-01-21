84cfe30318f15aa3f6bce73342f54490edb48855
Danil Tolstov <dfrontman@gmail.com>
2022-01-21 17:16:55 +0300

    binding and component hooks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule} from "@angular/forms";
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { ProductListComponent } from "./products/product-list.component";
-import {ConvertToSpacePipe} from "./shared/convert-to-space.pipe";
+import { ProductListComponent } from './products/product-list.component';
+import { ConvertToSpacePipe } from './shared/convert-to-space.pipe';
+import { StarComponent } from './shared/star.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
     ConvertToSpacePipe,
+    StarComponent,
   ],
   imports: [
     BrowserModule,
@@ -18,4 +20,5 @@ import {ConvertToSpacePipe} from "./shared/convert-to-space.pipe";
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/shared/convert-to-space.pipe.ts
+++ src/app/shared/convert-to-space.pipe.ts
@@ -1,4 +1,4 @@
-import {Pipe, PipeTransform} from "@angular/core";
+import { Pipe, PipeTransform } from '@angular/core';
 
 
 @Pipe({
@@ -7,6 +7,6 @@ import {Pipe, PipeTransform} from "@angular/core";
 
 export class ConvertToSpacePipe implements PipeTransform {
   transform(value: string, character: string): string {
-    return value.replace(character,' ')
+    return value.replace(character, ' ');
   }
 }
